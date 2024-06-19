dc09d80d5d1c77a75ffb88e870fca0f9b872808b
Tianpeng Gou <tgou1055@goumbp.local>
2024-06-19 20:38:33 +0930

    add parquet table

--- glue_job_script.py
+++ glue_job_script.py
@@ -5,6 +5,8 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.dynamicframe import DynamicFrame
 from awsglue.job import Job
+
+from pyspark.sql.types import StructType, StructField, BooleanType, ByteType, ShortType, IntegerType, StringType, FloatType, DoubleType
   
 sc = SparkContext.getOrCreate()
 glueContext = GlueContext(sc)
@@ -13,19 +15,24 @@ job = Job(glueContext)
 
 
 # Database and table names
-source_database = "cfn_imba_database"
+source_database = "cfn_imba_database_csv"
 source_table = "aisles"
-#target_table = "cfn_database.cfn_parquet_product_clearned"
-target_path = "s3://tgou1055-imba-sls-test-event/data-parquet/aisles/"
+target_path = "s3://tgou1055-imba-sls-event/data_parquet/aisles/"
 
 # Read data from the catalog
-dyf = glueContext.create_dynamic_frame.from_catalog(database = source_database, table_name = source_table)
+#dyf = glueContext.create_dynamic_frame.from_catalog(database = source_database, table_name = source_table)
 #dyf.printSchema()
-df = dyf.toDF()
+#df = dyf.toDF()
+
+
+aisles_schema = StructType([
+    StructField("aisle_id", IntegerType(), True),
+    StructField("aisle", StringType(), True)
+])
+aisles_df = spark.read.csv("s3://tgou1055-imba-sls-event/data_csv/aisles/aisles.csv", header=True, schema=aisles_schema)
+aisles_df.write.mode("overwrite").parquet(target_path)
 
 # Write data to s3 local with parquet
-#filtered_df.write.mode("overwrite").partitionBy("department_id").parquet(target_path)
-df.write.mode("overwrite").parquet(target_path)
 
 spark.stop()
 job.commit()
